79d6b4b62193f82045ff83f2627a7e55204bdcb5
Robert <71099420+rfreem2@users.noreply.github.com>
2022-02-02 16:42:04 -0500

    starting website
    getting website running

---
+++ src/Sorting_Algorithms/insertionsort.js
@@ -0,0 +1,32 @@
+// Insertion sort that outputs a 2D array consisting of every step required to sort te array.
+const insertionsort = (arr) => {
+    //If the array only has one number or is empty, return the array as an element in an array (return type is 2D array)
+    if (arr.length <= 1) return [arr];
+    let out = [];
+    let iLength = arr.length;
+
+    //Iterate through the array and shift each number into the new sorted array. 
+    for(let i = 0; i < iLength; i++)
+    {
+        console.log(arr);
+        //Recieve value to be inserted into new array well removing it from the old array.
+        let value = arr.shift();
+        console.log(value);
+
+        let j = i-1;
+
+        //In worst case, each insert operation into new array is proportional to the size of the output array.
+        while(j >= 0 && value < out[j]){
+            out[j+1] = out[j];
+            j--;
+        }
+
+        out[j+1] = value;
+
+        console.log(out);
+                
+        console.log("\n");
+    }
+}
+
+//insertionsort([1,8,4,32,15]);
