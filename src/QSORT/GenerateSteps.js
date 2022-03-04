1cea83183739b6ce119b0001d3d1484797161b0f
Jarrod Hasnain <jhasnain@uwo.ca>
2022-03-03 19:10:10 -0500

    Updated quicksort

--- src/QSORT/GenerateSteps.js
+++ src/QSORT/GenerateSteps.js
@@ -1,177 +1,147 @@
+let steps = [];
+let stepNo = 0;
+let sorted = -1;
+let thisArray = [];
+let globalI = -1;
+let globalJ = 0;
+let globalPivot = 0;
+
 export default function GenerateSteps(array) {
 
-    const steps = [{}];
+    let high = array.length - 1;
 
-    let stepNo = 0;
-    let i = -2;
-    let j = 0;
-    let pivot = array.length - 1;
-    let sorted = -1;
-    let divisions = [];
+    globalPivot = high;
 
-    while (sorted < array.length - 1) {
+    thisArray = [...array];
 
-        while (j < pivot) {
-            steps.push({
-                doIncI: false,
-                doIncJ: false,
-                doPartition: false,
-                doSwap: false,
-                doCompare: true,
-                doChangeIndex: false,
-                currentArray: array,
-                currentI: i,
-                currentJ: j,
-                currentPivot: pivot,
-                stepNumber: stepNo++,
-                desc: "Compare value at index J to pivot"
-            });
-            if (array[j] < array[pivot]) {
-                if (i !== -2) {
-                    steps.push({
-                        doIncI: true,
-                        doIncJ: false,
-                        doPartition: false,
-                        doSwap: false,
-                        doCompare: false,
-                        doChangeIndex: false,
-                        currentArray: array,
-                        currentI: i,
-                        currentJ: j,
-                        currentPivot: pivot,
-                        stepNumber: stepNo++,
-                        desc: "Value at J is less than pivot, therefore increment I"
-                    });
-                    i++;
-                    steps.push({
-                        doIncI: false,
-                        doIncJ: false,
-                        doPartition: false,
-                        doSwap: true,
-                        doCompare: false,
-                        doChangeIndex: false,
-                        currentArray: array,
-                        currentI: i,
-                        currentJ: j,
-                        currentPivot: pivot,
-                        stepNumber: stepNo++,
-                        desc: "After incrementing I, swap it with J"
-                    });
-                    let temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                    steps.push({
-                        doIncI: false,
-                        doIncJ: true,
-                        doPartition: false,
-                        doSwap: false,
-                        doCompare: false,
-                        doChangeIndex: false,
-                        currentArray: array,
-                        currentI: i,
-                        currentJ: j,
-                        currentPivot: pivot,
-                        stepNumber: stepNo++,
-                        desc: "Now increment J"
-                    });
-                    j++;
-                }
-            } else {
-                if (i !== -2) {
-                    steps.push({
-                        doIncI: false,
-                        doIncJ: true,
-                        doPartition: false,
-                        doSwap: false,
-                        doCompare: false,
-                        doChangeIndex: false,
-                        currentArray: array,
-                        currentI: i,
-                        currentJ: j,
-                        currentPivot: pivot,
-                        stepNumber: stepNo++,
-                        desc: "J was bigger than the pivot, increment J"
-                    });
-                    j++;
-                } else {
-                    for(let n = -1; n < j; n++) {
-                        steps.push({
-                            doIncI: false,
-                            doIncJ: true,
-                            doPartition: false,
-                            doSwap: false,
-                            doCompare: false,
-                            doChangeIndex: false,
-                            currentArray: array,
-                            currentI: i,
-                            currentJ: j,
-                            currentPivot: pivot,
-                            stepNumber: stepNo++,
-                            desc: "I hasn't been defined yet. Increment until it's at the index of J minus 1"
-                        });
-                        i = n;
-                    }
-                }
-            }
-        }
-        steps.push({
-            doIncI: true,
-            doIncJ: false,
-            doPartition: false,
-            doSwap: false,
-            doCompare: false,
-            doChangeIndex: false,
-            currentArray: array,
-            currentI: i,
-            currentJ: j,
-            currentPivot: pivot,
-            stepNumber: stepNo++,
-            desc: "J has reached the pivot. Increment I one more time"
-        });
-        i++;
-        steps.push({
-            doIncI: false,
-            doIncJ: false,
-            doPartition: true,
-            doSwap: false,
-            doCompare: false,
-            doChangeIndex: false,
-            currentArray: array,
-            currentI: i,
-            currentJ: j,
-            currentPivot: pivot,
-            stepNumber: stepNo++,
-            desc: "Partition array: Swap I with the Pivot and the new Pivot becomes the element below I"
-        });
-        let temp = array[i];
-        array[i] = array[pivot];
-        array[pivot] = temp;
-        pivot = i - 1;
-        i = -2;
-        j = sorted + 1;
-        divisions.push(pivot);
-        if (pivot === sorted + 1) {
-            steps.push({
-                doIncI: false,
-                doIncJ: false,
-                doPartition: false,
-                doSwap: false,
-                doCompare: false,
-                doChangeIndex: true,
-                currentArray: array,
-                currentI: i,
-                currentJ: j,
-                currentPivot: pivot,
-                stepNumber: stepNo++,
-                desc: "Pivot has reached the end of the array, this is sorted, move on to the next division"
-            });
-            sorted = pivot;
-            divisions.splice(divisions.length-1,1)
-            pivot = divisions[divisions.length - 1];
-            i = -2;
-            j = sorted + 1;
-        }
-    }
+    quicksort(thisArray, 0, high);
 
     return steps;
+}
+
+function partition(array, low, high) {
+
+    // Set the pivot value and initialize i and j
+    let pivotValue = array[high];
+    let i = low - 1;
+    for (let j = low; j < high; j++) {
+
+        // If j is less than or equal to the pivot value
+        if (array[j] <= pivotValue) {
+            // Increment i
+            globalI = i;
+            globalJ = j;
+            globalPivot = high;
+            steps.push({
+                "stepNo" : stepNo,
+                "sorted" : sorted,
+                "case" : determineStep(1),
+                "currentI" : globalI,
+                "currentJ" : globalJ,
+                "currentPivot" : globalPivot,
+                "currentArray" : [...thisArray]
+            });
+            stepNo++;
+
+            i++;
+
+            // Swap i and j
+            globalI = i;
+            globalJ = j;
+            globalPivot = high;
+            steps.push({
+                "stepNo" : stepNo,
+                "sorted" : sorted,
+                "case" : determineStep(3),
+                "currentI" : globalI,
+                "currentJ" : globalJ,
+                "currentPivot" : globalPivot,
+                "currentArray" : [...thisArray]
+            });
+            stepNo++;
+
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+        }
+
+        // Increment j
+        globalI = i;
+        globalJ = j;
+        globalPivot = high;
+        steps.push({
+            "stepNo" : stepNo,
+            "sorted" : sorted,
+            "case" : determineStep(0),
+            "currentI" : globalI,
+            "currentJ" : globalJ,
+            "currentPivot" : globalPivot,
+            "currentArray" : [...thisArray]
+        });
+        stepNo++;
+    }
+
+    // J has reached the pivot, now partition
+    globalI = i;
+    globalJ = high;
+    globalPivot = high;
+    steps.push({
+        "stepNo" : stepNo,
+        "sorted" : sorted,
+        "case" : determineStep(2),
+        "currentI" : globalI,
+        "currentJ" : globalJ,
+        "currentPivot" : globalPivot,
+        "currentArray" : [...thisArray]
+    });
+    i++;
+    let temp = array[i];
+    array[i] = array[high];
+    array[high] = temp;
+    stepNo++;
+
+    return i;
+}
+
+function quicksort(array, low, high) {
+
+    // If the J is less than the pivot
+    if (low < high) {
+
+        // Get the pivot index to partition the array
+        let pivotIndex = partition(array, low, high);
+
+        quicksort(array, low, pivotIndex - 1);
+        quicksort(array, pivotIndex + 1, high);
+    }
+    // Update the what's sorted
+    steps.push({
+        "stepNo" : stepNo,
+        "sorted" : sorted,
+        "case" : determineStep(4),
+        "currentI" : globalI,
+        "currentJ" : globalJ,
+        "currentPivot" : globalPivot,
+        "currentArray" : [...thisArray]
+    });
+    stepNo++;
+    sorted = high;
+}
+
+function determineStep(n) {
+    switch(n) {
+        case 0: 
+            return "increment j";
+        case 1:
+            return "increment i";
+        case 2:
+            return "partition";
+        case 3:
+            return "swap";
+        case 4:
+            return "change index";
+        default:
+            return "compare";
+    }
 }
