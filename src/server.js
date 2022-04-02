24f79acc0481d105c536e1b6a9e91377b3cbe783
Jarrod Hasnain <jhasnain@uwo.ca>
2022-04-01 22:33:08 -0400

    Changed more stuff to try and get it to deploy :(

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const auth = require('./middleware/auth');
