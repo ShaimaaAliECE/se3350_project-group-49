1072d1aff8505c356e45ae6d256e957aa3976c78
Jarrod Hasnain <jhasnain@uwo.ca>
2022-04-02 00:20:58 -0400

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://grand-verve-343121.ue.r.appspot.com/");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
